040476a56ef9c70700247a269f4d53685facecb7
dconquerorr <109660374+dconquerorr@users.noreply.github.com>
2022-07-24 04:00:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
-  access_key = "AKIA4I3EIGZGGDN22YNE"
-  secret_key = "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"
+  access_key = "AKIA4I3EIGZGCIIXFDKI"
+  secret_key = "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"
 }
